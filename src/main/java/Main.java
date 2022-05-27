b42d017433e76186605efa110a8d58f88f5947a7
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2022-05-27 07:51:07 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import com.cotrance.test.Window;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Window window = Window.get();
+        window.run();
+    }
+
+}
